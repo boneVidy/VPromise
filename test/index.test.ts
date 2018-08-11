f52e6ebfa6cc07d878b9e3aef96fc67dcec48fa1
OF2732 <wuweidi@qianmi.com>
2018-08-11 15:47:48 +0800

    setTimeout test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,20 +1,20 @@
 import { Vpromise } from "../src";
 
 describe('Vpromise test', () => {
-  it('should result is an array', async () => {
+  test('should result is an array', async () => {
     expect.assertions(1);
     const result = await Vpromise.all([1,2,3,4,5]);
     expect(result).toBeInstanceOf(Array);
 
   });
-  it('if params is not a PromiseLike Array  also can get results',async () => {
+  test('if params is not a PromiseLike Array  also can get results',async () => {
     expect.assertions(1);
     const result = await Vpromise.all([1,2,3,4,5]);
     expect(result).toMatchObject([1,2,3,4,5]);
 
   });
 
-  it('should defer.promise can return a promise', () => {
+  test('should use settimeout in Vpromise can return the correct value', async () => {
     const asyncGetNumber = () => {
       return new Vpromise((resolve, reject) => {
         setTimeout(() => {
@@ -22,11 +22,8 @@ describe('Vpromise test', () => {
         })
       })
     }
-    // expect.assertions(1);
-    asyncGetNumber().then((value) => {
-      expect(value).toBe(100);
-    })
-    
-
+    expect.assertions(1);
+    const result = await asyncGetNumber();
+    expect(result).toBe(100);
   });
 });
